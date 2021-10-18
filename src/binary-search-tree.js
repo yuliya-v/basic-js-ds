bc129ceeb8caea58b43e1e1b1cd06b5b8a19a2b1
Yuliya Volchek <yuliavolchek@gmail.com>
2021-10-18 20:43:19 +0300

    feat: bst - find method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,31 +6,69 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+class Node {
+  constructor(data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+  }
+}
+
 module.exports = class BinarySearchTree {
 
+  constructor() {
+    this.rootNode = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootNode = this.addNode(this.rootNode, data);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addNode(node, data) {
+    if (!node) return new Node(data);
+    if (node.data === data) return;
+    if (data < node.data) {
+      node.left = this.addNode(node.left, data);
+    }
+    if (data > node.data) {
+      node.right = this.addNode(node.right, data);
+    }
+    return node;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.search(this.rootNode, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  search(node, data) {
+    if (!node) return false;
+    if (data < node.data) {
+      return this.search(node.left, data);
+    }
+    if (data > node.data) {
+      return this.search(node.right, data);
+    }
+    return true;
+  }
+
+  find(data, node = this.rootNode) {
+    if (!node) return null;
+    if (data < node.data) {
+      return this.find(data, node.left);
+    }
+    if (data > node.data) {
+      return this.find(data, node.right);
+    }
+    return node;
+  }
+
+  remove(data) {
+    
   }
 
   min() {
